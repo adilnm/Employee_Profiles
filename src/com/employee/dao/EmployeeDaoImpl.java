5eb63a23e8cebc17c3174457716b9b7336519aae
Adil Naimi <adilnm23@gmail.com>
2020-12-13 15:03:04 -0600

    Create the servrlet to register

--- src/com/employee/dao/EmployeeDaoImpl.java
+++ src/com/employee/dao/EmployeeDaoImpl.java
@@ -1,12 +1,40 @@
 package com.employee.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.employee.entity.Employee;
+import com.employee.utils.DBConnectionUtil;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 
 	@Override
 	public boolean employeeRegistration(Employee employee) {
-		return false;
+		boolean success = false;
+		try {
+			Connection con = DBConnectionUtil.getConnection();
+			PreparedStatement ps = con.prepareStatement(
+					"INSERT INTO employee (FIRST_NAME, LAST_NAME, POSITION, CITY,PASSWORD) VALUES (?,?,?,?,?)");
+			ps.setString(1, employee.getFirstName());
+			ps.setString(2, employee.getLastName());
+			ps.setString(3, employee.getPosition());
+			ps.setString(4, employee.getCity());
+			ps.setString(5, employee.getPassword());
+			int rs = ps.executeUpdate();
+
+			if (rs > 0) {
+				success = true;
+			}
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return success;
 	}
 
 }
---
+++ src/com/employee/servlet/EmployeeRegistrationServlet.java
@@ -0,0 +1,34 @@
+package com.employee.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.employee.dao.EmployeeDao;
+import com.employee.dao.EmployeeDaoImpl;
+import com.employee.entity.Employee;
+
+@WebServlet("/register")
+public class EmployeeRegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String position = request.getParameter("position");
+		String city = request.getParameter("city");
+		String password = request.getParameter("password");
+
+		Employee emp = new Employee(firstName, lastName, position, city, password);
+
+		EmployeeDao dao = new EmployeeDaoImpl();
+
+		dao.employeeRegistration(emp);
+	}
+
+}
