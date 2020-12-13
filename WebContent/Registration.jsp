576a051080aae246946590baaba2463c86794ecf
Adil Naimi <adilnm23@gmail.com>
2020-12-13 13:36:10 -0600

    Create a profile for the employee & a registration page

---
+++ WebContent/Registration.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Registration</title>
+</head>
+<body>
+	<form action="register" method="post">
+		First Name: <input type="text" name="firstName"/><br/>
+		Last Name: <input type="text" name="lastName"/><br/>
+		Position: <input type="text" name="position"/><br/>
+		City: <input type="text" name="city"/><br/>
+		Password: <input type="password" name="password"/><br/>
+		<input type="submit">
+	</form>
+</body>
+</html>
---
+++ src/com/employee/entity/Employee.java
@@ -0,0 +1,68 @@
+package com.employee.entity;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String position;
+	private String city;
+	private String password;
+
+	public Employee(String firstName, String lastName, String position, String city, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.position = position;
+		this.city = city;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
