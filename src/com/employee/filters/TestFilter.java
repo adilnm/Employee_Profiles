c43eb63b1b5752c95e1cf7dbb44682680ca653f9
Adil Naimi <adilnm23@gmail.com>
2020-12-21 22:56:19 -0600

    Add more filters

--- src/com/employee/filters/TestFilter.java
+++ src/com/employee/filters/TestFilter.java
@@ -10,38 +10,23 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-/**
- * Servlet Filter implementation class TestFilter
- */
 @WebFilter("/*")
 public class TestFilter implements Filter {
 
-	/**
-	 * Default constructor.
-	 */
 	public TestFilter() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		System.out.println("My filter got called");
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
 		// TODO Auto-generated method stub
 	}
